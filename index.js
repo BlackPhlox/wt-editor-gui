262a4a24a57ce4373c23689d7e4195f2b71eb3b0
BlackPhlox <theluja@gmail.com>
2020-05-09 14:49:58 +0200

    Added opn when running npm start

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
-var connect = require("connect");
-var serveStatic = require("serve-static");
+const connect = require("connect");
+const serveStatic = require("serve-static");
+const opn = require('opn');
+
+const port = process.env.PORT || 8080;
 
 connect()
   .use(serveStatic(__dirname))
-  .listen(8080, () => console.log("Server running on 8080..."));
+  .listen(port, () => console.log("Server running on 8080..."));
+
+process.argv.forEach(function (val, index, array) {
+  if (index == 2 && (val == "-open" || val == "-o")) opn("http://localhost:" + port + "/");
+});
