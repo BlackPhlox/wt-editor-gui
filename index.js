7d6c695826460564beb81b7b1b4ddd099dc68fd6
Mikkel Luja Rasmussen <milr@itu.dk>
2020-05-15 18:29:09 +0200

    Added loading of settings.json

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const connect = require("connect");
 const serveStatic = require("serve-static");
 const opn = require('opn');
+const fs = require('fs');
+const config = require("app-settings")("config.json");
 
 const port = process.env.PORT || 8080;
 
@@ -8,6 +10,34 @@ connect()
   .use(serveStatic(__dirname))
   .listen(port, () => console.log("Server running on 8080..."));
 
-process.argv.forEach(function (val, index, array) {
+process.argv.forEach((val, index, array) => {
   if (index == 2 && (val == "-open" || val == "-o")) opn("http://localhost:" + port + "/");
 });
+
+
+
+//Load current settings.json
+
+
+read = function(path){
+  try{
+      let rawdata = fs.readFileSync(path);
+      return JSON.parse(rawdata);
+  } catch(err){
+      console.error("Parse error: No settings.json or config.json found, if there is a config.json file, the path to settings.json might be incorrect, else run setup.bat again");
+      throw err;
+  }
+}
+
+const jsonPath = config.jsonPath;
+let js = read(jsonPath);
+
+console.log(js);
+
+write = function(data,filename){
+  let sJson = JSON.stringify(data,null,4);
+  fs.writeFileSync(filename, sJson,{encoding:'utf8',flag:'w'});
+}
+
+write(js,"temp.json");
+
